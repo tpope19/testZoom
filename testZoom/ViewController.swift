97ad1a12570b53899078223880d1c62aaf21dfb5
Ted <tedpope929@gmail.com>
2018-10-08 09:59:56 -0500

    New code with pan

--- testZoom/ViewController.swift
+++ testZoom/ViewController.swift
@@ -12,14 +12,22 @@ class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var image: UIImageView!
-
     
     override func viewDidLoad() {
+        
         scrollView.contentSize = CGSize(width: 1600, height: 900)
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 5.0
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
     }
 
     override func didReceiveMemoryWarning() {
